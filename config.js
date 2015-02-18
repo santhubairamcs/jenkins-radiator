7e79c4b604579b347e5941e19a94cb873d37ff0c
Sean Van Osselaer <svo@qual.is>
2015-02-18 22:39:44 +1100

    Quick change to support loading jobs from 'build-monitor-plugin' RSS feed

--- config.js
+++ config.js
@@ -11,9 +11,27 @@ var configs = [
         refresh_interval: 24000,
         radiatorTitle: 'Sample 2',
         excludeFilter: [],
-        includeFilter: ["build3","build4"]
+        includeFilter: [],
+        dynamicIncludeFilter: true,
+        dynamicIncludeFilterUrl: "http://localhost:8081/radiator.xml"
     }
 ];
+
+function getConfigFor(config) {
+    config.includeFilter = [];
+    $.get(config.dynamicIncludeFilterUrl, function (data) {
+        $(data).find("entry").each(function () {
+            var jobName = $(this).find("title").text().split(" ")[0];
+            config.includeFilter.push(jobName);
+        });
+    });
+}
+
+configs.forEach(function(entry) {
+    if (entry.dynamicIncludeFilter === true) {
+        getConfigFor(entry);
+    }
+});
 
 var config = configs[0];
 
